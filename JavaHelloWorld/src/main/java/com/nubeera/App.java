9e93a738d042278bbdb9f54a5b4c9934e01f00c0
Kodiganti sudharshan <chennasusmitha14@gmail>
2023-04-10 10:02:42 +0530

    helloproject using git bash

---
+++ JavaHelloWorld/src/main/java/com/nubeera/App.java
@@ -0,0 +1,13 @@
+package com.nubeera;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
